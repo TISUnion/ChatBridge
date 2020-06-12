11db092ab8259f7ee92b46c631c53f9ea9e83983
Fallen_Breath <yqx791125833@gmail.com>
2020-06-12 22:06:43 +0800

    Update ChatBridge_discord.py

--- ChatBridge_discord.py
+++ ChatBridge_discord.py
@@ -107,7 +107,8 @@ class DiscordBot(commands.Bot):
 				if x < 1000:
 					break
 				x /= 1000
-				ret = '%.{}f'.format(4 - len(str(int(x)))) % x + c
+				if x < 1000:
+					ret = '%.{}f'.format(4 - len(str(int(x)))) % x + c
 			return str(ret)
 
 		self.log('Adding result "' + str((title, message)) + '" to Discord Bot')
